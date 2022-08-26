67abd1a0519630eab1cb79c94c0564c58a0d352d
code-simplifier-mahbub <Easycodermahbubwith5500@gmail.com>
2022-08-26 21:06:32 +0600

    multiline added

---
+++ MileStone-6/ES6/multiline.js
@@ -0,0 +1,12 @@
+//expensive way
+const multilineWithCoat = 'abbas \n' + 
+'keramot \n'+
+'rahim';
+
+//easy way
+const multilineWithBacktick = `
+abbas
+keramot
+rahim`;
+
+console.log(multilineWithCoat)
