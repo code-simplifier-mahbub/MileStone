1e9c8d79052c2b8acdab32823ea0a2d5b00c6e31
code-simplifier-mahbub <Easycodermahbubwith5500@gmail.com>
2022-09-03 07:57:54 +0600

    error handler for api

--- MileStone-6/API-example/async.js
+++ MileStone-6/API-example/async.js
@@ -1,7 +1,38 @@
 const displayUserData = () =>{
+    const url =  `https://randomuser.me/api/?gender=female`
     fetch(url)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => displayData(data.results[0]))
+    .catch(error => console.log(error))
 }
 
-displayUserData();
+const displayData = info => {
+    console.log(info.gender)
+}
+
+//asynchronous functioin
+
+// const loadUserAsync = async() => {
+//     const url =  `https://randomuser.me/api/?gender=female`;
+//     const res = await fetch(url);
+//     const data = await res.json();
+//     displayLoadUser(data.results[0]);
+// }
+
+//with error handler try catch
+const loadUserAsync = async() => {
+    const url =  `https://randomuser.me/api/?gender=female`;
+
+    try{
+        const res = await fetch(url);
+        const data = await res.json();
+        displayLoadUser(data.results[0]);
+    }
+    catch (error){
+        console.log(error)
+    }
+}
+
+const displayLoadUser = info => {
+    console.log(info.name.first)
+}
