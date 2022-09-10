3425cd3418bf997667027400986dfe5c06541ec1
code-simplifier-mahbub <Easycodermahbubwith5500@gmail.com>
2022-09-11 01:15:03 +0600

    pass by refference are added

---
+++ MileStone-7/common-js-concept/passby.js
@@ -0,0 +1,26 @@
+let x = 5; 
+let y = 4;
+
+function argumentsPass(){
+    x = 3;
+    const sum = x * y;
+    return sum;
+}
+
+// console.log(x)
+const result = argumentsPass(x, y);
+// console.log(x)
+
+
+let num1 = {name: 'mim', another: 'sheuly'};
+let num2 = {name: 'anika', another: 'unknown'}
+
+function namelike(one, two){
+    one.name = 'sava'
+    two.another = 'jamiya'
+    return one.name, two.another;
+}
+
+console.log(num1, num2)
+console.log(namelike(num1, num2))
+console.log(num1, num2)
